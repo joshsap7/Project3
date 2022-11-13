2a51a8c6754e96fc5c50617e1228eab38c15b5fa
nahomkifetew <82552955+nahomkifetew@users.noreply.github.com>
2022-11-13 01:31:40 -0500

    Added Doubly linkedList

---
+++ Project3/src/DLList.java
@@ -0,0 +1,323 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DLList<E> {
+    /**
+     * How many nodes are in the list
+     */
+    private int size;
+
+    /**
+     * The first node in the list. THIS IS A SENTINEL NODE AND AS SUCH DOES NOT
+     * HOLD ANY DATA. REFER TO init()
+     */
+    private Node<E> head;
+
+    /**
+     * The last node in the list. THIS IS A SENTINEL NODE AND AS SUCH DOES NOT
+     * HOLD ANY DATA. REFER TO init()
+     */
+    private Node<E> tail;
+
+    /**
+     * Create a new DLList object.
+     */
+    public DLList() {
+        init();
+    }
+
+
+    /**
+     * Initializes the object to have the head and tail nodes
+     */
+    private void init() {
+        head = new Node<E>(null);
+        tail = new Node<E>(null);
+        head.setNext(tail);
+        tail.setPrevious(head);
+        size = 0;
+    }
+
+
+    /**
+     * Checks if the array is empty
+     *
+     * @return true if the array is empty
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    /**
+     * Gets the number of elements in the list
+     *
+     * @return the number of elements
+     */
+    public int size() {
+        return size;
+    }
+
+
+    /**
+     * Removes all of the elements from the list
+     */
+    public void clear() {
+        init();
+    }
+
+
+    /**
+     * Checks if the list contains the given object
+     *
+     * @param obj
+     *            the object to check for
+     * @return true if it contains the object
+     */
+    public boolean contains(E obj) {
+        return lastIndexOf(obj) != -1;
+    }
+
+
+    /**
+     * Gets the object at the given position
+     *
+     * @param index
+     *            where the object is located
+     * @return The object at the given position
+     * @throws IndexOutOfBoundsException
+     *             if there no node at the given index
+     */
+    public E get(int index) {
+        return getNodeAtIndex(index).getData();
+    }
+
+
+    /**
+     * 
+     * @param data
+     * @return E
+     *         the last element
+     */
+    @SuppressWarnings("unchecked")
+    public E add(E obj) {
+
+        Node<E> node = new Node<E>(obj);
+        node.setPrevious(head);
+        node.setNext(head.next());
+        head.next().setPrevious(node);
+        head.setNext(node);
+
+        Node<E> last_element = tail.previous();
+        last_element.previous().setNext(tail);
+        tail.setPrevious(last_element.previous());
+
+        return (E)last_element.getData();
+    }
+
+
+    /**
+     * Adds the object to the position in the list
+     *
+     * @param index
+     *            where to add the object
+     * @param obj
+     *            the object to add
+     * @throws IndexOutOfBoundsException
+     *             if index is less than zero or greater than size
+     * @throws IllegalArgumentException
+     *             if obj is null
+     */
+    public void add(int index, E obj) {
+        if (index < 0 || size < index) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (obj == null) {
+            throw new IllegalArgumentException("Cannot add null "
+                + "objects to a list");
+        }
+
+        Node<E> nodeAfter;
+        if (index == size) {
+            nodeAfter = tail;
+        }
+        else {
+            nodeAfter = getNodeAtIndex(index);
+        }
+
+        Node<E> addition = new Node<E>(obj);
+        addition.setPrevious(nodeAfter.previous());
+        addition.setNext(nodeAfter);
+        nodeAfter.previous().setNext(addition);
+        nodeAfter.setPrevious(addition);
+        size++;
+
+    }
+
+
+    /**
+     * gets the node at that index
+     * 
+     * @param index
+     * @return node at index
+     */
+    private Node<E> getNodeAtIndex(int index) {
+        if (index < 0 || size() <= index) {
+            throw new IndexOutOfBoundsException("No element exists at "
+                + index);
+        }
+        Node<E> current = head.next(); // as we have a sentinel node
+        for (int i = 0; i < index; i++) {
+            current = current.next();
+        }
+        return current;
+    }
+
+
+    /**
+     * Take a node, and remove it from its current position in the list, and
+     * move it to the head of the list.
+     * 
+     * @param node
+     *            The node to promote
+     */
+    private void prioritize(Node<E> node) {
+        // Remove from the current position
+        deleteNode(node);
+
+        // Insert at head.
+        node.setPrevious(head);
+        node.setNext(head.next());
+        head.next().setPrevious(node);
+        head.setNext(node);
+    }
+
+
+    /**
+     * Makes given node a head node
+     * 
+     * @param data
+     *            data that will be the new head
+     * @return tail
+     */
+    public E makeHead(E data) {
+        // Always insert on null insertions.
+        if (data == null) {
+            return add(data);
+        }
+
+        Node<E> node = head;
+        while ((node = node.next()) != tail) {
+            if (data.equals(node.getData())) {
+                prioritize(node);
+                return null;
+            }
+        }
+
+        return add(data);
+    }
+
+
+    /**
+     * Remove a node from the list
+     * 
+     * @param node
+     *            The node to remove
+     */
+    private void deleteNode(Node<E> node) {
+        // Remove from the current position
+        node.next().setPrevious(node.previous());
+        node.previous().setNext(node.next());
+    }
+
+
+    /**
+     * Gets the last time the given object is in the list
+     *
+     * @param obj
+     *            the object to look for
+     * @return the last position of it. -1 If it is not in the list
+     */
+    public int lastIndexOf(E obj) {
+        /*
+         * We should go from the end of the list as then we an stop once we find
+         * the first one
+         */
+        Node<E> current = tail.previous();
+        for (int i = size() - 1; i >= 0; i--) {
+            if (current.getData().equals(obj)) {
+                return i;
+            }
+            current = current.previous();
+        }
+        return -1; // if we do not find it
+    }
+
+
+    /**
+     * Removes the element at the specified index from the list
+     *
+     * @param index
+     *            where the object is located
+     * @throws IndexOutOfBoundsException
+     *             if there is not an element at the index
+     * @return true if successful
+     */
+    public boolean remove(int index) {
+        Node<E> nodeToBeRemoved = getNodeAtIndex(index);
+        nodeToBeRemoved.previous().setNext(nodeToBeRemoved.next());
+        nodeToBeRemoved.next().setPrevious(nodeToBeRemoved.previous());
+        size--;
+        return true;
+    }
+
+
+    /**
+     * Removes the first object in the list that .equals(obj)
+     *
+     * @param obj
+     *            the object to remove
+     * @return true if the object was found and removed
+     */
+
+    public boolean remove(E obj) {
+        Node<E> current = head.next();
+        while (!current.equals(tail)) {
+            if (current.getData().equals(obj)) {
+                current.previous().setNext(current.next());
+                current.next().setPrevious(current.previous());
+                size--;
+                return true;
+            }
+            current = current.next();
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns a string representation of the list If a list contains A, B, and
+     * C, the following should be returned "{A, B, C}" (Without the quotations)
+     *
+     * @return a string representing the list
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{");
+        if (!isEmpty()) {
+            Node<E> currNode = head.next();
+            while (currNode != tail) {
+                E element = currNode.getData();
+                builder.append(element.toString());
+                if (currNode.next() != tail) {
+                    builder.append(", ");
+                }
+                currNode = currNode.next();
+            }
+        }
+
+        builder.append("}");
+        return builder.toString();
+    }
+
+}
